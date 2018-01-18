630385565051bbaf4a70bfb38a9f60257203b045
Pablo Vent <Ventp.work@gmail.com>
2018-01-18 22:22:43 +0000

    callback issue fixed

--- script.js
+++ script.js
@@ -74,22 +74,36 @@ view = {
     displayTodos: function () {
         var todosUl = document.querySelector('ul');
         todosUl.innerHTML = '';
-        for (var i = 0; i < todoList.todos.length; i++) {
+        // for (var i = 0; i < todoList.todos.length; i++) {
+        //     var todoLi = document.createElement('li');
+        //     var todoTextWithCompletion = '';
+        //     var todo = todoList.todos[i];
+
+        //     if (todo.completed === true) {
+        //         todoTextWithCompletion = '(X)' + todo.todoText;
+        //     } else {
+        //         todoTextWithCompletion = '( )' + todo.todoText;
+        //     }
+
+        //     todoLi.id = i;
+        //     todoLi.textContent = todoTextWithCompletion;
+        //     todoLi.appendChild(this.createDeleteButton());
+        //     todosUl.appendChild(todoLi);
+        // }
+        todoList.todos.forEach(function(todo, position) {
             var todoLi = document.createElement('li');
-            var todoTextWithCompletion = '';
-            var todo = todoList.todos[i];
-
-            if (todo.completed === true) {
-                todoTextWithCompletion = '(X)' + todo.todoText;
-            } else {
-                todoTextWithCompletion = '( )' + todo.todoText;
-            }
-
-            todoLi.id = i;
-            todoLi.textContent = todoTextWithCompletion;
-            todoLi.appendChild(this.createDeleteButton());
-            todosUl.appendChild(todoLi);
-        }
+                var todoTextWithCompletion = '';
+                if (todo.completed === true) {
+                    todoTextWithCompletion = '(X)' + todo.todoText;
+                } else {
+                    todoTextWithCompletion = '( )' + todo.todoText;
+                }
+    
+                todoLi.id = position;
+                todoLi.textContent = todoTextWithCompletion;
+                todoLi.appendChild(this.createDeleteButton());
+                todosUl.appendChild(todoLi); 
+        }, this);
     },
     createDeleteButton: function () {
         var deleteButton = document.createElement('button');
